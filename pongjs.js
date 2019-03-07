0616f8470076e304a5149d51d3253caa0810ac41
Felicidad Abad Quintanilla <fabadq@beta12.aulas.gsyc.urjc.es>
2019-03-07 10:48:38 +0100

    Animación de la bola

--- pongjs.js
+++ pongjs.js
@@ -8,6 +8,17 @@ function main()
 
   var ctx = canvas.getContext("2d");
 
+  window.onkeydown = (e) => {
+    e.preventDefault();
+
+    console.log(e.key);
+    
+    if (e.key == "a") {
+      console.log("tecla a pressed")
+    }
+  }
+
+/*
   // Raquetas
   ctx.fillStyle = "white";
   ctx.fillRect(50, 50, 10, 40);
@@ -44,5 +55,73 @@ function main()
   ctx.fillStyle = 'white'
   ctx.fillText("2", 250, 60);
   ctx.fillText("0", 320, 60);
+*/
 
+  // Animamos la pelota
+  var bola = {
+
+    x : 0,
+    y : 0,
+
+    x_init : 50,
+    y_init : 50,
+
+    xspeed : 10,
+    yspeed : 1,
+
+    ctx: null,
+    width : 5,
+    height : 5,
+
+    reset : function() {
+      this.x = this.x_init;
+      this.y = this.y_init;
+    },
+
+    init : function(ctx) {
+      console.log("Bola: Init");
+      this.reset();
+      this.ctx = ctx;
+    },
+
+    draw : function() {
+      console.log("Bola: Draw");
+      this.ctx.fillStyle = "white";
+      this.ctx.fillRect(this.x, this.y, this.width, this.height);
+    },
+
+    update : function() {
+      console.log("Bola: Update");
+      this.x = this.x + this.xspeed;
+      this. y = this.y + this.yspeed;
+    }
+  }
+
+  bola.init(ctx);
+  bola.draw();
+
+  var timer = null;
+  var sacar = document.getElementById("sacar");
+
+  sacar.onclick = () => {
+    if (!timer) {
+      // Lanzar timer si es que no estaba ya lanzado
+      timer = setInterval(() => {
+        console.log("aiuda");
+        // Cada 20ms, actualizamos bola
+        bola.update()
+        // Borramos canvas
+        ctx.clearRect(0,0,canvas.width, canvas.height);
+        // Dibujo la bola
+        bola.draw()
+        // Condición de terminación
+        if (bola.x > canvas.width) {
+          clearInterval(timer);
+          timer = null;
+          bola.reset();
+          bola.draw();
+        }
+      }, 20);
+    }
+  }
 }
