17c7f1d52f8168855b738d0b0d04efa69a10cba7
Felicidad Abad Quintanilla <fabadq@beta12.aulas.gsyc.urjc.es>
2019-03-14 10:48:30 +0100

    Bola rebota en los bordes. Falta mover las raquetas.

--- pongjs.js
+++ pongjs.js
@@ -12,19 +12,48 @@ function main()
     e.preventDefault();
 
     console.log(e.key);
-    
+
     if (e.key == "a") {
       console.log("tecla a pressed")
     }
   }
 
-/*
   // Raquetas
-  ctx.fillStyle = "white";
-  ctx.fillRect(50, 50, 10, 40);
-  ctx.fillRect(500, 300, 10, 40);
+  function raqueta(x_init, y_init, upkey, downkey) {
 
+    this.color = "white";
+    this.x_init = x_init;
+    this.y_init = y_init;
+    this.x = 0;
+    this.y = 0;
+    this.yspeed = 5;
+    this.upkey = null;
+    this.downkey = null;
 
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x_init, this.y_init, 10, 40);
+
+    this.draw = function () {
+      ctx.fillRect(this.x_init, this.y, 10, 40);
+    }
+
+    this.update = function () {
+      window.onkeydown = (e) => {
+        e.preventDefault();
+        console.log(e.key);
+
+        if (e.key == this.upkey) {
+          console.log("probando");
+          this.y = this.y + this.yspeed;
+          ctx.fillRect(this.x_init, this.y, 10, 40);
+        }
+      }
+    }
+  }
+
+  var raqueta1 = new raqueta(50, 50, "w", "s");
+  var raqueta2 = new raqueta(500, 300, "ArrowUp", "ArrowDown");
+/*
   // Dibujamos el círculo
   ctx.beginPath();
   //-- Dibujar un circulo: coordenadas x,y del centro
@@ -33,6 +62,7 @@ function main()
   //-- Dibujar el relleno
   ctx.fillStyle = 'white';
   ctx.fill()
+*/
 
   // Lineas verticales
   ctx.beginPath();
@@ -55,7 +85,7 @@ function main()
   ctx.fillStyle = 'white'
   ctx.fillText("2", 250, 60);
   ctx.fillText("0", 320, 60);
-*/
+
 
   // Animamos la pelota
   var bola = {
@@ -66,7 +96,7 @@ function main()
     x_init : 50,
     y_init : 50,
 
-    xspeed : 10,
+    xspeed : 5,
     yspeed : 1,
 
     ctx: null,
@@ -92,8 +122,22 @@ function main()
 
     update : function() {
       console.log("Bola: Update");
+      if (this.x > canvas.width) {
+        this.xspeed = -this.xspeed;
+      } else if (this.x < 0) {
+        this.xspeed = this.xspeed * -1;
+        console.log(this.xspeed)
+      }
+
+      // REBOTA UN POCO RARO, REVISAR
+      if (this.y > canvas.height) {
+        this.yspeed = -this.xspeed;
+      } else if (this.x < 0) {
+        this.yspeed = this.yspeed * -1;
+      }
+
       this.x = this.x + this.xspeed;
-      this. y = this.y + this.yspeed;
+      this.y = this.y + this.yspeed;
     }
   }
 
@@ -115,12 +159,6 @@ function main()
         // Dibujo la bola
         bola.draw()
         // Condición de terminación
-        if (bola.x > canvas.width) {
-          clearInterval(timer);
-          timer = null;
-          bola.reset();
-          bola.draw();
-        }
       }, 20);
     }
   }
